7be4a602ce445676e562334ad114741cbd1d114d
Chris M <webmaster@magnaws.com>
2018-08-19 20:53:12 -0400

    BAOBAB RELEASE

---
+++ doc/search/functions_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['delete_5fdata',['delete_data',['../class_r_v_p___p_h_p___s_d_k.html#abf86e9437ac69b55a698d62f16296e69',1,'RVP_PHP_SDK']]],
+  ['description',['description',['../class_r_v_p___p_h_p___s_d_k___thing.html#a5c46fb6f231cf347080b6019437bb5e6',1,'RVP_PHP_SDK_Thing']]],
+  ['distance',['distance',['../class_a___r_v_p___p_h_p___s_d_k___data___object.html#a8cf5f63ffb7f3ee8e0ae2011e7aa19cb',1,'A_RVP_PHP_SDK_Data_Object']]]
+];
---
+++ doc/search/functions_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['name',['name',['../class_a___r_v_p___p_h_p___s_d_k___object.html#a4b516aaa5fa38da4fed24ab6001627e2',1,'A_RVP_PHP_SDK_Object']]],
+  ['nickname',['nickname',['../class_r_v_p___p_h_p___s_d_k___user.html#a566bd6137e1cf711526d95dae2c6eeaf',1,'RVP_PHP_SDK_User']]]
+];
