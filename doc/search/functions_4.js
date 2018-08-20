1d5623d0c9857f790c53f9a65a8dc6ef1af63724
Chris M <webmaster@magnaws.com>
2018-08-19 20:49:07 -0400

    BAOBAB RELEASE

--- doc/search/functions_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['delete_5fdata',['delete_data',['../class_r_v_p___p_h_p___s_d_k.html#abf86e9437ac69b55a698d62f16296e69',1,'RVP_PHP_SDK']]],
-  ['distance',['distance',['../class_a___r_v_p___p_h_p___s_d_k___data___object.html#a8cf5f63ffb7f3ee8e0ae2011e7aa19cb',1,'A_RVP_PHP_SDK_Data_Object']]]
-];
